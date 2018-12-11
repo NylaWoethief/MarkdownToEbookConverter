af27e7aa31bb2952241628c5945ec1fe2a6ecdc0
Nyla Woethief <34950472+NylaWoethief@users.noreply.github.com>
2018-12-11 01:58:46 +0000

    Create BookConverter.sh

---
+++ BookConverter.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# This script converts a Markdown file into
+# ODT, PDF, and EPUB
+
+# Get information about book
+
+InformationCorrect="No"
+Yes='[Yy][Ee]*[Ss]*'
+No='[Nn][Oo]*'
+
+read -p "Please enter path to Markdown file: " MarkdownFile
+read -p "Please enter the book's title: " Title
+read -p "Please enter author name: " Penname
+read -p "Please type copyright information (eg. Creative Commons Attribution 4.0): " Copyright
+read -p "Please type language code (eg. en for English, es for Spanish, fr for French): " LanguageCode
+read -p "Do you have a cover image? (YES/NO): " CheckForCover
+
+printf "You entered:\n Markdown file: $MarkdownFile\n Title: $Title\n Name: $Penname\n Copyright: $Copyright\n Language: $LanguageCode\n Cover Image: $CheckForCover\n"
+
+TitlePlusName=$Penname-$Title
+TitlePlusNameNoSpaces="$(echo $TitlePlusName | tr -d ' ')"
+mkdir $TitlePlusNameNoSpaces
+
+# Create metadata file
+# Do not remove the whitespace in echo
+
+TitleMetadata="$Title-metadata.txt"
+touch "$TitlePlusNameNoSpaces/$TitleMetadata"
+echo "---
+
+
+
+
+
+
+..." > "$TitlePlusNameNoSpaces/$TitleMetadata"
+
+
+CoverAnswered="False"
+
+while [[ $CoverAnswered == "False" ]]; do
+  if [[ $CheckForCover == $Yes ]]; then
+    read -p "Please type file path for cover image: " BookCoverPath
+    CoverAnswered="True"
+
+    # Add cover image to $TitleMetadata
+
+    sed -i "7s/.*/cover-image: $BookCoverPath/" "$TitlePlusNameNoSpaces/$TitleMetadata"
+
+  elif [[ $CheckForCover == $No ]]; then
+    CoverAnswered="True"
+  else read -p "I did not understand your answer. Do you have a cover image? (YES/NO): " CheckForCover
+  fi
+done
+
+# Add date to $TitleMetadata
+# Uses British date format
+
+CurrentDate="$(date +%d" "%B" "%Y)"
+
+sed -i "5s/.*/date: $CurrentDate/" "$TitlePlusNameNoSpaces/$TitleMetadata"
+
+# Add user-input to $TitleMetadata
+
+sed -i "2s/.*/title: $Title/" "$TitlePlusNameNoSpaces/$TitleMetadata"
+sed -i "3s/.*/author: $Penname/" "$TitlePlusNameNoSpaces/$TitleMetadata"
+sed -i "4s/.*/rights: $Copyright/" "$TitlePlusNameNoSpaces/$TitleMetadata"
+sed -i "6s/.*/lang: $LanguageCode/" "$TitlePlusNameNoSpaces/$TitleMetadata"
+
+# Convert MarkdownFile to other formats
+
+pandoc $MarkdownFile --pdf-engine=pdflatex -o "$TitlePlusNameNoSpaces/$TitlePlusNameNoSpaces.pdf" --table-of-contents
+pandoc -s -f markdown -t odt -o "$TitlePlusNameNoSpaces/$TitlePlusNameNoSpaces.odt" $MarkdownFile
+pandoc -s -f markdown -t epub -o "$TitlePlusNameNoSpaces/$TitlePlusNameNoSpaces.epub" "$TitlePlusNameNoSpaces/$TitleMetadata" $MarkdownFile --table-of-contents
+
+# Strip metadata from odt
+
+mat "$TitlePlusNameNoSpaces/$TitlePlusNameNoSpaces.odt" --backup
+rm "$TitlePlusNameNoSpaces/$TitlePlusNameNoSpaces.odt.bak"
+
+# Remove metadata file because it is no longer needed
+
+rm "$TitlePlusNameNoSpaces/$TitleMetadata"
